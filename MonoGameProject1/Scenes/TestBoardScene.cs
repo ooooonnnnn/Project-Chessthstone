e4da0713293c2429813bccfa30fbe905f226f4d8
OmriTafla <omritafla@gmail.com>
2025-08-07 15:35:13 +0300

    board shows at screen center

--- MonoGameProject1/Scenes/TestBoardScene.cs
+++ MonoGameProject1/Scenes/TestBoardScene.cs
@@ -9,6 +9,7 @@ public class TestBoardScene : Scene
 		ChessBoard board = new ChessBoard("");
 		board.transform.SetScaleFromFloat(0.2f);
 		board.transform.origin = Vector2.One * board.totalWidth * 0.5f;
+		board.transform.parentSpacePos = GameManager.Graphics.Viewport.Bounds.Center.ToVector2();
 		
 		gameObjects = [board];
 	}
