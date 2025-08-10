48490bdad70ee8de08c8cd8d72c5e24940edbb8b
OmriTafla <omritafla@gmail.com>
2025-08-10 13:48:00 +0300

    GameState expansion

--- MonoGameProject1/Chess Stuff/GameState.cs
+++ MonoGameProject1/Chess Stuff/GameState.cs
@@ -6,7 +6,4 @@ namespace MonoGameProject1;
 /// <summary>
 /// Records a state of the game.
 /// </summary>
-public record GameState
-{
-	public bool isWhiteTurn { get; init; }
-}
+public record GameState(bool isWhiteTurn, List<Point> blackPieces, List<Point> whitePieces);
