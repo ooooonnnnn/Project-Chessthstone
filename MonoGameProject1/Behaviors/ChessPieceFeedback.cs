b517375208bc2d2e6efeaf9516115bad5ab71472
OmriTafla <omritafla@gmail.com>
2025-08-18 14:35:22 +0300

    remove logs

--- MonoGameProject1/Behaviors/ChessPieceFeedback.cs
+++ MonoGameProject1/Behaviors/ChessPieceFeedback.cs
@@ -6,18 +6,12 @@ public class ChessPieceFeedback : Behavior
 {
     public override void Initialize()
     {
-        Console.WriteLine("CHESS PIECE FEEDBACK INITIALIZED");
         if (gameObject is not ChessPiece chessPiece)
         {
             throw new Exception("ChessPieceFeedback can only be used with a Sprite GameObject.");
         }
 
-        chessPiece.OnGetHit += () =>
-        {
-            Console.WriteLine("I TRIED SO HARD TO PLAY THE SOUND");
-            AudioManager.PlaySound(AudioClips.HitSound);
-            Console.WriteLine("I PLAYED THE SOUND");
-        };
+        chessPiece.OnGetHit += () => AudioManager.PlaySound(AudioClips.HitSound);
         chessPiece.OnDeath += _ => AudioManager.PlaySound(AudioClips.DeathSound, 0.7f);
         chessPiece.OnMove += () => AudioManager.PlaySound(AudioClips.MoveSound);
         chessPiece.OnTeleport += () => AudioManager.PlaySound(AudioClips.TeleportSound, 0.7f);
